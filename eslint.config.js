d4f51a63f7915c8b08d7ebdabb4a70ba6e687c45
shunya9811 <11wash27@gmail.com>
2024-11-26 19:45:19 +0900

    prettierの設定の追加

--- eslint.config.js
+++ eslint.config.js
@@ -4,7 +4,7 @@ import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
 
-export default tseslint.config(
+export default [ tseslint.config(
   { ignores: ['dist'] },
   {
     extends: [js.configs.recommended, ...tseslint.configs.recommended],
@@ -25,4 +25,6 @@ export default tseslint.config(
       ],
     },
   },
-)
+),
+eslintConfiPrettier,
+]
