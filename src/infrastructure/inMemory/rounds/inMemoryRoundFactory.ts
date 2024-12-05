c615f20e9bcc01872d9e7b2463929a3187f640a3
a2311kk <166212224+a2311kk@users.noreply.github.com>
2024-12-05 23:29:38 +0900

    RoundFactory を実装

---
+++ src/infrastructure/inMemory/rounds/inMemoryRoundFactory.ts
@@ -0,0 +1,19 @@
+import { Round } from "@/domain/models/rounds/round";
+import { RoundFactory } from "@/domain/models/rounds/roundFactory";
+import { RoundId } from "@/domain/models/rounds/roundId";
+import { ShoeId } from "@/domain/models/shoes/shoeId";
+
+/**
+ * インメモリラウンドファクトリ
+ */
+export class InMemoryRoundFactory implements RoundFactory {
+  /**
+   * ラウンドを生成する
+   *
+   * @param shoeId シュー ID
+   * @returns ラウンド
+   */
+  public create(shoeId: ShoeId): Round {
+    return new Round(new RoundId(crypto.randomUUID()), shoeId);
+  }
+}
