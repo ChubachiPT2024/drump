3e454bec4462e7c859c0c4782f5e38ea52c97afc
a2311kk <166212224+a2311kk@users.noreply.github.com>
2024-12-08 22:44:10 +0900

    テストの修正

--- src/application/matches/matchApplicationService.test.ts
+++ src/application/matches/matchApplicationService.test.ts
@@ -9,6 +9,7 @@ import { ShoeId } from "@/domain/models/shoes/shoeId";
 import { MatchAddRoundCommand } from "./AddRound/matchAddRoundCommand";
 import { Round } from "@/domain/models/rounds/round";
 import { RoundId } from "@/domain/models/rounds/roundId";
+import { Hand } from "@/domain/models/hands/hand";
 
 describe("create", () => {
   test("Can create a match", async () => {
@@ -31,7 +32,12 @@ describe("add round", () => {
     const match = new Match(new MatchId("matchId"), new ShoeId("shoeId"), []);
     await matchRepository.saveAsync(match);
     const service = new MatchApplicationService(matchFactory, matchRepository);
-    const round = new Round(new RoundId("roundId"), match.shoeId);
+    const round = new Round(
+      new RoundId("roundId"),
+      match.shoeId,
+      new Hand([], false),
+      new Hand([], false),
+    );
 
     // Act
     await service.addRoundAsync(
