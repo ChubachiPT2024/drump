fd1ee4a5ce0f8a2037a05ff704cb11e5f52023d6
a2311kk <166212224+a2311kk@users.noreply.github.com>
2024-12-13 20:39:38 +0900

    サンプルルータを実装

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,9 @@
 import express from "express";
 import ViteExpress from "vite-express";
+import { SampleRouter } from "./router/sampleRouter";
 
 const app = express();
 
-app.get("/message", (_, res) => { res.send("Hello from express!") });
+app.use("/api/sample", SampleRouter.create());
 
 ViteExpress.listen(app, 3000, () => console.log("Server is listening..."));
