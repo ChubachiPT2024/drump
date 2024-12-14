c7a79b18da73288b2b76a578a2a9b9696f64de35
a2311kk <166212224+a2311kk@users.noreply.github.com>
2024-12-14 22:09:00 +0900

    ブラックジャックの主要な処理のうち、シューに関する API を実装

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,26 @@
 import express from "express";
 import ViteExpress from "vite-express";
 import { SampleRouter } from "./router/sampleRouter";
+import { InMemoryShoeFactory } from "./infrastructure/inMemory/shoes/inMemoryShoeFactory";
+import { InMemoryShoeRepository } from "./infrastructure/inMemory/shoes/inMemoryShoeRepository";
+import { CardsService } from "./domain/services/cardsService";
+import { ShoeApplicationService } from "./application/shoes/shoeApplicationService";
+import { ShoeRouterFactory } from "./router/shoes/shoeRouterFactory";
+
+// TODO DI フレームワークの検討
+const shoeFactory = new InMemoryShoeFactory();
+const shoeRepository = new InMemoryShoeRepository();
+const cardsService = new CardsService();
+const shoeApplicationService = new ShoeApplicationService(
+  shoeFactory,
+  shoeRepository,
+  cardsService,
+);
+const shoeRouterFactory = new ShoeRouterFactory(shoeApplicationService);
 
 const app = express();
 
 app.use("/api/sample", SampleRouter.create());
+app.use("/api/shoes", shoeRouterFactory.create());
 
 ViteExpress.listen(app, 3000, () => console.log("Server is listening..."));
