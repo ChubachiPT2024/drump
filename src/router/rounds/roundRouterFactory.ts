e3d988b19edfe73c7aba655eb5ecec54cce59ad3
a2311kk <166212224+a2311kk@users.noreply.github.com>
2024-12-14 23:38:27 +0900

    RoundApplicationService.startAsync メソッドに対応する Web API を実装

--- src/router/rounds/roundRouterFactory.ts
+++ src/router/rounds/roundRouterFactory.ts
@@ -1,5 +1,6 @@
 import { RoundCreateCommand } from "@/application/rounds/Create/roundCreateCommand";
 import { RoundApplicationService } from "@/application/rounds/roundApplicationService";
+import { RoundStartCommand } from "@/application/rounds/Start/roundStartCommand";
 import { Router } from "express";
 
 /**
@@ -29,6 +30,17 @@ export class RoundRouterFactory {
         const result = await this.roundApplicationService.createAsync(command);
 
         res.status(201).json(result);
+      } catch (err) {
+        next(err);
+      }
+    });
+
+    router.post("/start", async (req, res, next) => {
+      try {
+        const command = new RoundStartCommand(req.body.id);
+        await this.roundApplicationService.startAsync(command);
+
+        res.status(204).send();
       } catch (err) {
         next(err);
       }
