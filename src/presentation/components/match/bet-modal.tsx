0333ab11fdc731314d1f2867ae4a5c27b8ad1149
Tadashi Masuda <13a19u4a@gmail.com>
2024-12-31 17:10:35 +0900

    betmodal修正：クレジット以上のベット不可能

--- src/presentation/components/match/bet-modal.tsx
+++ src/presentation/components/match/bet-modal.tsx
@@ -11,15 +11,17 @@ import {
 } from "@/presentation/shadcnUI/components/ui/dialog";
 import { Label } from "@/presentation/shadcnUI/components/ui/label";
 import { Input } from "@/presentation/shadcnUI/components/ui/input";
+import { ResultSummary } from "../../types/resultSummary";
 
 interface BetModalProps {
   matchId: string;
   onClickDeal: (matchId: string, betAmount: number) => void;
+  credit: Pick<ResultSummary, "player">["player"]["credit"];
 }
 
 // TODO: ベット機能を実現するためにプレイヤーの情報を含んだpropsを追加
-export const BetModal = ({ onClickDeal, matchId }: BetModalProps) => {
-  const [betAmount, setBetAmount] = useState<number>(0);
+export const BetModal = ({ onClickDeal, matchId, credit }: BetModalProps) => {
+  const [betAmount, setBetAmount] = useState<number | undefined>(undefined);
 
   const isOpen = useBetModal((state) => state.isOpen);
   const onClose = useBetModal((state) => state.onClose);
@@ -36,11 +38,19 @@ export const BetModal = ({ onClickDeal, matchId }: BetModalProps) => {
           </DialogHeader>
           <div className="text-center flex flex-col space-y-2">
             {/* TODO: チップでの選択 */}
-            <Label htmlFor="bet" className="text-lg">
-              Bet Amount: {betAmount}
-            </Label>
-            {/* TODO: プレイヤーのクレジットをもとにして最大値を設定 */}
-            <Input type="number" min="0" max="100" />
+            <Label className="text-lg">Credit: {credit}</Label>
+            <Label className="text-lg">Bet Amount: {betAmount}</Label>
+            <Input
+              type="number"
+              min={0}
+              max={credit}
+              onChange={(e) => setBetAmount(Number(e.target.value))}
+            />
+            {betAmount && credit < betAmount && (
+              <Label className="text-red-500">
+                You don't have enough credit
+              </Label>
+            )}
           </div>
           <div className="flex justify-around">
             <div className="border-2 border-yellow-500 rounded-md">
@@ -48,9 +58,11 @@ export const BetModal = ({ onClickDeal, matchId }: BetModalProps) => {
                 className="rounded-sm"
                 size="default"
                 onClick={() => {
+                  if (!betAmount) return;
                   onClickDeal(matchId, betAmount);
                   onClose();
                 }}
+                disabled={!betAmount || credit < betAmount}
               >
                 <span className="text-base md:text-xl">DEAL</span>
               </Button>
